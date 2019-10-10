ac2e132c5d13066f8c709d9ecb110f52c72f4abb
make711 <marco91414@gmail.com>
2019-10-10 12:15:14 -0700

    add session detail page

--- src/containers/SessionDetail/index.tsx
+++ src/containers/SessionDetail/index.tsx
@@ -1,34 +1,71 @@
 import React, { useEffect, useState } from 'react';
-import jsonp from 'jsonp';
+import { request } from '../../utils/api';
+import { match as Match } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-
-const baseUrl = 'https://demo1-webservice.eventbase.com/v4/admin/events/frontendcodechallenge/sessions';
-const auth = '?api=cc1-0befd4410327ac7b8c7f88e4ed466e87d6f78eff29de81c3ee4e28d79b604eb2-0c75664d5c8211b4395e7f766a415a25827c7cf2';
+interface Session {
+  attachments: unknown[];
+  category_ids: string[];
+  categories: Array<{ id: string, name: string }>;
+  child_session_ids: string[];
+  child_sessions: unknown[];
+  description: string;
+  entities: Array<{ id: string, name: string }>;
+  entity_ids: string[]
+  external_id: string;
+  favoritable: boolean;
+  id: number;
+  image_url: string;
+  location: { id: string, name: string };
+  location_id: string;
+  name: string;
+  persistent: boolean;
+  subtitle: string;
+  tba: boolean;
+  thumbnail_image_url: string;
+  time_start: string;
+  time_stop: string;
+  visible: boolean;
+}
 
 interface Props {
   sessionId: number;
+  match: Match<{ id: string }>;
 }
 
 const SessionDetail: React.FC<Props> = (props) => {
-  const { sessionId } = props;
-  const [session, setSession] = useState([]);
+  const { match: { params: { id }} } = props;
+  const [session, setSession] = useState<Session>(null as any);
   useEffect(() => {
-    const fetchData = async () => {
-      jsonp(baseUrl+''+auth, (err, data) => {
-        if(err) {
-          console.log(err);
-        } else {
-          setSession(data.data);
-        }
-      });
-    }
-
-    fetchData();
-  }, [])
-
+    request(`/sessions/${id}`, {}, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        setSession(data.data);
+      }
+    });
+  }, [id])
+  if (!session) {
+    return null;
+  }
   return (
     <>
-    <h1>session detail</h1>
+      <h1>Session detail</h1>
+      <div>
+        <div>Name: {session.name}</div>
+        <div>Location: {session.location && session.location.name}</div>
+        <div>Description: {session.description}</div>
+        <div>Categories: {session.categories && session.categories.map((c) => c.name).join(', ')}</div>
+        <div>Start: {session.time_start}</div>
+        <div>End: {session.time_stop}</div>
+        <div style={{marginTop: '20px'}}>Child Sessions</div>
+        <ul>
+          {session.child_session_ids.map((id) => {
+            return <li><Link to={`/session/${id}`}>{id}</Link></li>;
+          })}
+        </ul>
+        <div><Link to={`/`}>Back</Link></div>
+      </div>
     </>
   );
 }
